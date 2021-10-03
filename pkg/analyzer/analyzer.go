aa6bfc6de6b6edfdaaca25979b52762381d986df
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-10-03 10:31:36 +0900

    feat: add linter for coding rule of const and var

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,19 +1,68 @@
 package analyzer
 
 import (
+	"fmt"
+	"go/ast"
+	"strings"
+
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 const doc = "code-rule-linter is analyzer that detects violations of coding rule"
 
 var Analyzer = &analysis.Analyzer{
-	Name:     "coding-rule-linter",
+	Name:     "codingRuleLinter",
 	Doc:      doc,
 	Run:      run,
 	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.FuncDecl)(nil),
+		(*ast.GenDecl)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
+		switch n := node.(type) {
+		case *ast.GenDecl:
+			problem, ok := validateGenDecl(n)
+			if ok {
+				return
+			}
+			pass.Reportf(n.Pos(), problem)
+		case *ast.FuncDecl:
+			if strings.HasSuffix(pass.Fset.File(n.Pos()).Name(), "_test.go") {
+				// TODO
+				fmt.Printf("funcdecl: %+v\n", n)
+			}
+			return
+		}
+	})
 	return nil, nil
+}
+
+func validateGenDecl(n *ast.GenDecl) (string, bool) {
+	const reportDoc string = "identifier should follow the coding rules"
+	for _, spec := range n.Specs {
+		switch s := spec.(type) {
+		case *ast.ValueSpec:
+			for _, n := range s.Names {
+				if n.Obj.Kind == ast.Var {
+					if !applyVarNamingRule(n.Obj.Name) {
+						return fmt.Sprintf("%s %s", n.Obj.Kind, reportDoc), false
+					}
+				} else if n.Obj.Kind == ast.Con {
+					if !applyConstNamingRule(n.Obj.Name) {
+						return fmt.Sprintf("%s %s", n.Obj.Kind, reportDoc), false
+					}
+				}
+			}
+		}
+	}
+	return "", true
 }
--- testdata/src/p/p.go
+++ testdata/src/p/p.go
@@ -1,1 +1,12 @@
 package p
+
+var _ int = 1
+var sample int = 2
+var sample1 int = 3 // want "var identifier should follow the coding rules"
+var Sample int = 4  // want "var identifier should follow the coding rules"
+
+const SAMPLE int = 1
+const SAMPLE_SAMPLE int = 2
+const _SAMPLE int = 3
+const SamPle int = 4  // want "const identifier should follow the coding rules"
+const SAMPLE1 int = 5 // want "const identifier should follow the coding rules"
